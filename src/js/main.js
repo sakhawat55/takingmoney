0ef7a07b901b6c5f6f96ecbb868f3513b45ea2bd
Sakhawat <sakhawat20055@gmail.com>
2019-12-06 22:57:49 +0600

    modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,36 @@
 function takingMoney(){
   var taka = document.getElementById("taka").value;
   if(taka == "" || taka<0){
-    document.getElementById("img-gif").setAttribute("src","src/img/0.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.a').css('display','block');
   }
   else if(taka == 0){
-    document.getElementById("img-gif").setAttribute("src","src/img/1.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.b').css('display','block');
   }
   else if(taka>=1 && taka<=9){
-    document.getElementById("img-gif").setAttribute("src","src/img/2.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.c').css('display','block');
   }
   else if(taka>=10 && taka<=49){
-    document.getElementById("img-gif").setAttribute("src","src/img/3.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.d').css('display','block');
   }
   else if(taka>=50 && taka<=99){
-    document.getElementById("img-gif").setAttribute("src","src/img/4.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.e').css('display','block');
   }
   else if(taka>=100 && taka<=499){
-    document.getElementById("img-gif").setAttribute("src","src/img/5.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.f').css('display','block');
   }
   else if(taka>=500 && taka<=999){
-    document.getElementById("img-gif").setAttribute("src","src/img/6.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.g').css('display','block');
   }
   else if(taka>=1000){
-    document.getElementById("img-gif").setAttribute("src","src/img/7.gif");
+    $('.img-gif').css('display','none');
+    $('.img-gif.h').css('display','block');
   }
 }
 
