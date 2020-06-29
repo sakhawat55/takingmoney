c9d04270a801c48c9cadc2292f6cf38754accec9
Sakhawat <sakhawat20055@gmail.com>
2020-06-29 15:17:28 +0600

    modified

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,55 @@
 function takingMoney(){
-  var taka = document.getElementById("taka").value;
+  var taka = document.getElementById('taka').value;
+  var emoticon = document.getElementsByClassName('img-gif');
+
   if(taka == "" || taka<0){
-    $('.img-gif').css('display','none');
-    $('.img-gif.a').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[0].style.display = 'block'
   }
   else if(taka == 0){
-    $('.img-gif').css('display','none');
-    $('.img-gif.b').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[1].style.display = 'block'
   }
   else if(taka>=1 && taka<=9){
-    $('.img-gif').css('display','none');
-    $('.img-gif.c').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[2].style.display = 'block'
   }
   else if(taka>=10 && taka<=49){
-    $('.img-gif').css('display','none');
-    $('.img-gif.d').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[3].style.display = 'block'
   }
   else if(taka>=50 && taka<=99){
-    $('.img-gif').css('display','none');
-    $('.img-gif.e').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[4].style.display = 'block'
   }
   else if(taka>=100 && taka<=499){
-    $('.img-gif').css('display','none');
-    $('.img-gif.f').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[5].style.display = 'block'
   }
   else if(taka>=500 && taka<=999){
-    $('.img-gif').css('display','none');
-    $('.img-gif.g').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[6].style.display = 'block'
   }
   else if(taka>=1000){
-    $('.img-gif').css('display','none');
-    $('.img-gif.h').css('display','block');
+    for(i = 0; i < 7; i++){
+      emoticon[i].style.display = 'none'
+    }
+    emoticon[7].style.display = 'block'
   }
 }
 
-document.getElementById("give-btn").addEventListener("click",takingMoney);
+document.getElementById('give-btn').addEventListener('click',takingMoney);
